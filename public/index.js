52d49d2abfcb599718d2870d88ada697519f7fe5
Fabian Kanhai <fabian.k@ziggo.nl>
2024-05-16 07:55:53 +0200

    Add public/index.js and linked to index.html

---
+++ public/index.js
@@ -0,0 +1,16 @@
+console.log('index script!');
+
+// let preDiv = document.createElement('div')
+// let text = document.createElement('p')
+// text.textContent = 'Yo, this the created content!'
+// preDiv.appendChild(text)
+
+// // let preDivStyle = document.style.cssText = ""
+// let preDivStyles = ["w-96", "h-64", "bg-indigo-700", "text-white", "text-center"]
+// preDivStyles.forEach(preDivStyle => {
+    
+//     preDiv.classList.add(preDivStyle)
+    
+//     return preDiv
+// })
+// document.body.prepend(preDiv)
